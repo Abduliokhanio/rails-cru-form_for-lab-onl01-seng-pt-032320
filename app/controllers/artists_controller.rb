8381371027e8c98110dd1504ef50bae22013c2f5
Abdul Khan <akhan17@outlook.com>
2020-07-23 21:15:21 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,15 +2,15 @@ class ArtistsController < ApplicationController
   def index
     @artists = Artist.all
   end
-  
-  def show 
+
+  def show
     @artist = Artist.find(params[:id])
   end
 
-  def new 
-    @artist = Artist.new 
+  def new
+    @artist = Artist.new
   end
-  
+
   def create
     @artist = Artist.new(artist_params(:name, :bio))
     @artist.save
@@ -22,11 +22,11 @@ class ArtistsController < ApplicationController
     @artist.update(artist_params(:name, :bio))
     redirect_to artist_path(@artist)
   end
-  
+
   def edit
     @artist = Artist.find(params[:id])
   end
-  
+
   private
 
   def artist_params(*args)
